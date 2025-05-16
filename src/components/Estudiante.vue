6184cced7fe85e420e0fe38d4117f43d764e8e01
Ajhurtado <ajhurtadop@uce.edu.ec>
2025-05-15 22:38:18 -0500

    Tarea3

--- src/components/Estudiante.vue
+++ src/components/Estudiante.vue
@@ -6,12 +6,22 @@
     <label for="id_apellido">Apellido:</label>
     <input v-model="nuevoApellido" id="id_apellido" type="text" >
 
+    <label for="id_edad">Edad:</label>
+    <input v-model="nuevaEdad" id="id_edad" type="number" >
+
+    <label for="id_genero">Genero:</label>
+    <input v-model="nuevoGenero" id="id_genero" type="text" >
+
+    <label for="id_estado">Estado:</label>
+    <input v-model="nuevoEstado" id="id_estado" type="text" >
+
+
     <button v-on:click="agregarEstudiante()" >Agregar Estudiante</button>
     {{ nuevoNombre }}
     <ul>
        <!-- <li v-for="estu in lista" :key="estu.nombre"> Nombre: {{ estu.nombre }} - Apellido: {{ estu.apellido }}</li> -->
-        <li v-for="{ nombre, apellido} in lista" :key="nombre">
-             Nombre: {{ nombre }} - Apellido: {{ apellido }}</li>
+        <li v-for="{ nombre, apellido, edad, genero, estado} in lista" :key="nombre">
+             Nombre: {{ nombre }} - Apellido: {{ apellido }} - Edad: {{ edad }} - Genero: {{ genero }} - Estado: {{ estado }}</li>
        
     </ul>
   </div>
@@ -24,6 +34,9 @@ export default {
             //propiedad reactiva para el nombre
             nuevoNombre: "Nuevo",
             nuevoApellido: "Nuevo",
+            nuevaEdad: "Nuevo",
+            nuevoGenero: "Nuevo",
+            nuevoEstado: "Nuevo",
             //propiedad reactiva de tipo arreglo
             lista: [{nombre:'Alexis',apellido:'Hurtado'},
                     {nombre:'Juan',apellido:'Pérez'},
@@ -31,7 +44,8 @@ export default {
                     {nombre:'María',apellido:'López'},
                     {nombre:'Ana',apellido:'Martínez'},
                     {nombre:'Luis',apellido:'Fernández'},
-                    {nombre:'Jose',apellido:'Ramirez'},
+                    {nombre:'Jose',apellido:'Ramirez'},          
+                    
                     ],
         };
     },
@@ -41,6 +55,9 @@ export default {
             const nuevo= {
                 nombre: this.nuevoNombre,
                 apellido: this.nuevoApellido,
+                edad: this.nuevaEdad,
+                genero: this.nuevoGenero,
+                estado: this.nuevoEstado,
             }
             //agregar un nuevo estudiante a la lista
             //this.lista.unshift(nuevo);
